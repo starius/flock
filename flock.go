2fb2dcea4f536364a63d66fe8b7d52afffce66b8
Boris Nagaev <bnagaev@gmail.com>
2017-03-13 04:35:35 +0100

    add docstrings

--- flock.go
+++ flock.go
@@ -4,10 +4,13 @@ import (
 	"os"
 )
 
+// LockFile places an exclusive lock on the file.
+// If the file is already locked, exists with error.
 func LockFile(f *os.File) error {
 	return LockFd(f.Fd())
 }
 
+// UnlockFile removes an existing lock held by this process.
 func UnlockFile(f *os.File) error {
 	return UnlockFd(f.Fd())
 }
--- flock_unix.go
+++ flock_unix.go
@@ -6,10 +6,13 @@ import (
 	"syscall"
 )
 
+// LockFile places an exclusive lock on the file.
+// If the file is already locked, exists with error.
 func LockFd(fd uintptr) error {
 	return syscall.Flock(int(fd), syscall.LOCK_EX|syscall.LOCK_NB)
 }
 
+// UnlockFile removes an existing lock held by this process.
 func UnlockFd(fd uintptr) error {
 	return syscall.Flock(int(fd), syscall.LOCK_UN|syscall.LOCK_NB)
 }
