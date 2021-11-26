41983f66ca4fc84709d62873f67627dc6c5c09d0
Boris Nagaev <bnagaev@gmail.com>
2021-11-26 07:58:01 +0000

    use x/sys instead of syscall on Unix platforms
    This commit fixes build on GOOS=solaris and GOOS=illumos.

--- flock_unix.go
+++ flock_unix.go
@@ -4,16 +4,16 @@
 package flock
 
 import (
-	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 // LockFile places an exclusive lock on the file.
 // If the file is already locked, exists with error.
 func LockFd(fd uintptr) error {
-	return syscall.Flock(int(fd), syscall.LOCK_EX|syscall.LOCK_NB)
+	return unix.Flock(int(fd), unix.LOCK_EX|unix.LOCK_NB)
 }
 
 // UnlockFile removes an existing lock held by this process.
 func UnlockFd(fd uintptr) error {
-	return syscall.Flock(int(fd), syscall.LOCK_UN|syscall.LOCK_NB)
+	return unix.Flock(int(fd), unix.LOCK_UN|unix.LOCK_NB)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/starius/flock
 
 go 1.15
+
+require golang.org/x/sys v0.0.0-20211124211545-fe61309f8881
